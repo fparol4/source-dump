c931bc146b706a6f1a8ff3c5c266ef29f44fd2f6
= <=>
2024-10-09 21:04:19 -0300

    feat: add lock-manager

---
+++ src/core/managers/lock-manager.ts
@@ -0,0 +1,22 @@
+type LockCallback = () => void
+
+export class LockManager {
+    public locked: boolean = false
+    private queue: Array<LockCallback> = []
+    
+    public async lock(): Promise<void> {
+        if (this.locked) {
+            /** Elegant way to manage the next lock possibility using a queue */
+            await new Promise(resolve => this.queue.push(resolve as LockCallback))
+        }
+        this.locked = true 
+    }
+
+    public async unlock(): Promise<void> {
+        this.locked = false
+        /** Get the next lock request and releases it */ 
+        const resolver = this.queue.shift()
+        if (resolver) resolver()
+    }
+}   
+
