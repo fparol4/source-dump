abb8f0c1ba110678b4d4129e8c3e8ec61d3a34dc
= <=>
2024-10-09 21:03:23 -0300

    chore: add project main types

---
+++ src/types.ts
@@ -0,0 +1,78 @@
+import { OffsetManager } from "./core/managers/offset-manager"
+
+/** Storages */
+export type Storage = {
+    connect: () => Promise<boolean>
+}
+
+export type WriteStorage = Storage & {
+    write<T>(normalized: [T]): Promise<boolean>
+    normalize<T>(denormalized: [object]): [T]
+}
+
+export type ReadPagination = {
+    limit: number
+    offset: number
+}
+
+export type ReadStorage = Storage & {
+    query(range: ReadPagination): Promise<[object]>
+}
+
+export type SourceStreamStorages = {
+    readStorage: ReadStorage
+    writeStorage: WriteStorage
+}
+
+/** SourceStream */
+export type SourceStreamContextParams = {
+    maxWorkers: number
+    chunkSize: number
+}
+
+export type SourceStreamContext = {
+    storages: SourceStreamStorages
+    params: SourceStreamContextParams
+}
+
+/** WorkerContext */
+export type WorkerContext = {
+    storages: SourceStreamStorages
+    pagination: ReadPagination
+}
+
+export type Worker = {
+    idx: number
+    processing: boolean // @TODO: The lock on the worker
+    start: () => void
+}
+
+export type WorkersManager = {
+    workers: [Worker]
+    getAvailable: () => Promise<Worker | undefined>  
+}
+
+export type WorkerStatus = 'completed' | 'error' | 'end'
+export type WorkerMessage = {
+    workeridx: number 
+    status: WorkerStatus
+    pagination: ReadPagination
+    errors?: [string]
+}
+
+/** ProcessContext */
+export type OffsetHistory = WorkerMessage
+
+export type OffsetManagerContext = {
+    chunkSize: number 
+    startOffset?: number 
+    history?: OffsetHistory[]
+}
+
+export type ProcessContext = {
+    workersManager: WorkersManager  
+    offsetManager: OffsetManager
+}
+
+/** Application */
+export type HandledReturn = [boolean, null | Error]
